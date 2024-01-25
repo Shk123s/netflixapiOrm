c07b901264a6d97743ce0ab7903e25e103aa0281
Shk123s <shaqeebsk1234@gmail.com>
2024-01-25 17:22:51 +0530

    second

--- database.js
+++ database.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"password",
-    database:"netflixdb",
+    password:"12345",
+    database:"netflix",
     port:3306
 })
 connection.connect(function (err) {
--- index.js
+++ index.js
@@ -1,34 +1,115 @@
 const express = require("express");
 const connection = require("./database");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json({ type: "application/json" }));
+const getuser = async (req, res) => {
+  try {
+    const [results] = await connection.promise().query("select * from users");
+    res.send(results);
+    console.log(results);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const Addusers = async (req, res) => {
+  try {
+    const { email, password, names, is_active, created_at } = req.body;
+    console.log(req.body);
+    let queryStrng = `insert into users(email ,password ,names,is_active,created_at) values(? ,? ,?,? ,?)`;
+    const [results] = await connection
+      .promise()
+      .query(queryStrng, [email, password, names, is_active, created_at]);
+    res.status(201).send({
+      message: "user successfully added ",
+      results,
+    });
+    console.log(results);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "error while operation",
+    });
+  }
+};
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    //  console.log(id)
+    const { email, password, names, is_active, created_at } = req.body;
+    let queryStrng = `update   users set email=? ,password=? ,names=?,is_active=?,created_at=? where id=? `;
+    const [results] = await connection
+      .promise()
+      .query(queryStrng, [email, password, names, is_active, created_at, id]);
 
-const getuser =async ()=>{
-  try {
-    const [results] = await  connection.promise().query("select * from users")
-    res.send(results)
-    console.log(results)
-    
+    res.status(201).send({
+      message: "user updated  added ",
+      results,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    res.status(500).send({
+      message: "error while operation",
+      error,
+    });
   }
-}
-const Addusers =async ()=>{
+};
+const deleteUser = async (req, res) => {
   try {
- const  { email ,password ,names,created_at ,updated_at }   = req.body
-    const [results] = await  connection.promise().query(" insert into  users  ")
-    res.send(results)
-    console.log(results)
-    
+    const { id } = req.params;
+    const queryStrng = "delete from users where id=?";
+    const results = await connection.promise().query(queryStrng, [id]);
+    //  console.log(results)///
+    if (results[0].affectedRows === 0) {
+      res.status(404).send({
+        message: "user Not found",
+      });
+    } else {
+      res.status(200).send({
+        message: "user deleted successfully",
+        results,
+      });
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    res.status(501).send({
+      message: "user not found",
+      error,
+    });
   }
-}
+};
+const getUserbyid = async(req,res)=>{
+  try {
+    const {id} = req.params;
+    const queryStrng = "select * from users where id=?";
+    const [results] = await connection.promise().query(queryStrng,[id]);
+
+    if (results.length === 0 ) {
+      res.send({message:" user not found"})
+    }
+  else
+  {
+    res.status(200).send(
+      {
+        message:"user found",
+        result:results[0]
+      }
+    )
+  }
+   // console.log(results[0])
+  } catch (error) {
+    res.status(500).send(
+      {
+        message:"internal server error"
+      }
+    )
+  }
+} 
 app.post("/users", Addusers);
 app.get("/users", getuser);
-app.patch("/users", Addusers);
-app.delete("/users", getuser);
-
-
+app.put("/users/:id", updateUser);
+app.delete("/users/:id", deleteUser);
+app.get("/users/:id", getUserbyid);
 
 
 app.listen(3000, () => {
